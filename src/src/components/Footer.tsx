5e58f4e7fd53d03f382ddc013f460f351ef86a69
007sunny <sunny.singhal@joshtechnologygroup.com>
2023-12-05 10:56:34 +0530

    EFS-695 Minor fix

--- src/src/components/Footer.tsx
+++ src/src/components/Footer.tsx
@@ -11,12 +11,12 @@ function Footer({ links, requestId }: { links?: TBundleLink[], requestId?: Strin
     const [username, setUserName] = useState<string|undefined>();
     const [scope, setScope] = useState<string|undefined>();
     const [page, setPage] = useState(1);
-    const url: string = location.pathname;
+    const url: string = window.location.pathname;
     const hasPrev: boolean = page > 1;
     const hasNext: boolean = links ? links.some((link: TBundleLink) => link.relation === 'next') : false;
 
     useEffect(() => {
-        const queryParams = new URLSearchParams(location?.search);
+        const queryParams = new URLSearchParams(window.location?.search);
         // number of pages skipped + 1 is the current page
         setPage(parseInt(queryParams.get('_getpagesoffset') || '0') + 1);
     }, []);
@@ -27,7 +27,7 @@ function Footer({ links, requestId }: { links?: TBundleLink[], requestId?: Strin
     }, [userDetails]);
 
     const handleChange = (_event: React.ChangeEvent<unknown>, newPage: number) => {
-        const queryParams = new URLSearchParams(location?.search);
+        const queryParams = new URLSearchParams(window.location?.search);
         if (newPage > 1) {
             queryParams.set('_getpagesoffset', `${newPage - 1}`);
             setPage(newPage);
@@ -35,7 +35,7 @@ function Footer({ links, requestId }: { links?: TBundleLink[], requestId?: Strin
             queryParams.delete('_getpagesoffset');
             setPage(1);
         }
-        navigate(location.pathname + (queryParams.size && '?') + queryParams.toString());
+        navigate(window.location.pathname + (queryParams.size && '?') + queryParams.toString());
     };
 
     return (
