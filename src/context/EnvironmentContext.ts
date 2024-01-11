59b1b35d1a7f90a9bc788b369a3f3050e288e0e8
007sunny <sunny.singhal@joshtechnologygroup.com>
2024-01-11 15:58:11 +0530

    EFS-678 Minor change

--- src/context/EnvironmentContext.ts
+++ src/context/EnvironmentContext.ts
@@ -8,9 +8,6 @@ const EnvContext = createContext<{
     AUTH_CODE_FLOW_CLIENT_ID: string,
     AUTH_CODE_FLOW_URL: string,
     AUTH_ENABLED: boolean,
-    ENV: string,
-    ENVIRONMENT: string,
-    FHIR_SERVER_URL: string,
 }>({
     fhirUrl: process.env.REACT_APP_FHIR_SERVER_URL || '',
     AUTH_CUSTOM_GROUP: process.env.REACT_APP_AUTH_CUSTOM_GROUP || '',
@@ -18,9 +15,6 @@ const EnvContext = createContext<{
     AUTH_CODE_FLOW_CLIENT_ID: process.env.REACT_APP_AUTH_CODE_FLOW_CLIENT_ID || '',
     AUTH_CODE_FLOW_URL: process.env.REACT_APP_AUTH_CODE_FLOW_URL || '',
     AUTH_ENABLED: isTrue(process.env.REACT_APP_AUTH_ENABLED || '1'),
-    ENV: process.env.REACT_APP_ENV || '',
-    ENVIRONMENT: process.env.REACT_APP_ENVIRONMENT || '',
-    FHIR_SERVER_URL: process.env.REACT_APP_FHIR_SERVER_URL || '',
 });
 
 export default EnvContext;
