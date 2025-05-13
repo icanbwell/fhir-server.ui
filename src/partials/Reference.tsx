44a76f753f88082ea2d396087c982c67625191b0
Imran Qureshi <imranq2@hotmail.com>
2025-05-12 19:46:20 -0700

    fix formatting for Reference.tsx

--- src/partials/Reference.tsx
+++ src/partials/Reference.tsx
@@ -4,57 +4,66 @@ import { TBaseResourceProps } from '../types/baseTypes';
 import { TExtension } from '../types/partials/Extension';
 import { IdentifierSystem } from '../utils/identifierSystem';
 import { TReference } from '../types/partials/Reference';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 type TReferenceProps = TBaseResourceProps & {
-  reference: any;
-  field?: string;
+    reference: any;
+    field?: string;
 };
 
 function Reference({ reference: references = [], name, field }: TReferenceProps) {
-  const [uuidReferences, setUuidReferences] = useState<TReference[]>(
-    []
-  );
+    const [uuidReferences, setUuidReferences] = useState<TReference[]>([]);
 
-  useEffect(() => {
-    // Ensure that references is an array
-    let referenceArray = Array.isArray(references) ? references : [references];
+    useEffect(() => {
+        // Ensure that references is an array
+        let referenceArray = Array.isArray(references) ? references : [references];
 
-    setUuidReferences(
-      referenceArray
-        .map((reference: any) => {
-          let uuidReference: string | undefined;
-          if (field) {
-            uuidReference = reference[`${field}`]?.extension?.find(
-              (e: TExtension) => e.url === IdentifierSystem.uuid
-            )?.valueString;
-          } else {
-            uuidReference = reference?.extension?.find(
-              (e: TExtension) => e.url === IdentifierSystem.uuid
-            )?.valueString;
-          }
-          return { reference: uuidReference, display: reference.display };
-        })
-        .filter((u: TReference) => u.reference)
-    );
-  }, [references]);
+        setUuidReferences(
+            referenceArray
+                .map((reference: any) => {
+                    let uuidReference: string | undefined;
+                    if (field) {
+                        uuidReference = reference[`${field}`]?.extension?.find(
+                            (e: TExtension) => e.url === IdentifierSystem.uuid
+                        )?.valueString;
+                    } else {
+                        uuidReference = reference?.extension?.find(
+                            (e: TExtension) => e.url === IdentifierSystem.uuid
+                        )?.valueString;
+                    }
+                    return { reference: uuidReference, display: reference.display };
+                })
+                .filter((u: TReference) => u.reference)
+        );
+    }, [references]);
 
-  return uuidReferences && uuidReferences.length > 0 && uuidReferences[0] ? (
-    <Box>
-      <Typography variant="h4">{name}</Typography>
-      {uuidReferences.map((reference: TReference, index: Number) =>
-        reference ? (
-          <Link
-            href={`/4_0_0/${reference.reference}`}
-            key={`${index}`}
-            display='block'
-            mb={index !== references.length - 1 ? 1 : 0}
-          >
-            {reference.display || reference.reference}
-          </Link>
-        ) : null
-      )}
-    </Box>
-  ) : null;
+    return uuidReferences && uuidReferences.length > 0 && uuidReferences[0] ? (
+        <Box>
+            <Typography variant="h4">{name}</Typography>
+            {uuidReferences.map((reference: TReference, index: Number) =>
+                reference ? (
+                    <Link
+                        href={`/4_0_0/${reference.reference}`}
+                        key={`${index}`}
+                        rel="noopener noreferrer"
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            gap: 1,
+                            textDecoration: 'none',
+                            '&:hover': {
+                                textDecoration: 'underline',
+                            },
+                        }}
+                    >
+                        <Typography>{reference.display || reference.reference}</Typography>
+
+                        <OpenInNewIcon />
+                    </Link>
+                ) : null
+            )}
+        </Box>
+    ) : null;
 }
 
 export default Reference;
