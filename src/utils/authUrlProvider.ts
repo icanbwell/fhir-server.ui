72b62a6a92edca8b716151b40d7bbbd940e098a7
Imran Qureshi <imranq2@hotmail.com>
2025-05-15 11:54:36 -0700

    remove unnecessary properties

--- src/utils/authUrlProvider.ts
+++ src/utils/authUrlProvider.ts
@@ -10,12 +10,6 @@ class AuthUrlProvider {
         logoutUrl?: string;
         wellKnownUrl?: string;
     }> {
-        const customUserName =
-            process.env[`REACT_APP_AUTH_${provider.toUpperCase()}_CUSTOM_USERNAME`];
-        const customGroup = process.env[`REACT_APP_AUTH_${provider.toUpperCase()}_CUSTOM_GROUP`];
-        const customScope = process.env[`REACT_APP_AUTH_${provider.toUpperCase()}_CUSTOM_SCOPE`];
-        const clientId = process.env[`REACT_APP_AUTH_${provider.toUpperCase()}_CLIENT_ID`];
-
         const wellKnownUrl = process.env[`REACT_APP_AUTH_${provider.toUpperCase()}_WELL_KNOWN_URL`];
         let authorizeUrl: string | undefined;
         let tokenUrl: string | undefined;
@@ -36,23 +30,6 @@ class AuthUrlProvider {
             logoutUrl = process.env[`REACT_APP_AUTH_${provider.toUpperCase()}_LOGOUT_URL`];
         }
 
-        const tokenToSendToFhirServer =
-            process.env[`REACT_APP_AUTH_${provider.toUpperCase()}_TOKEN_TO_SEND_TO_FHIR_SERVER`];
-
-        if (!customUserName) {
-            throw new Error(
-                `REACT_APP_AUTH_${provider.toUpperCase()}_CUSTOM_USERNAME is not defined`
-            );
-        }
-        if (!customGroup) {
-            throw new Error(`REACT_APP_AUTH_${provider.toUpperCase()}_CUSTOM_GROUP is not defined`);
-        }
-        if (!customScope) {
-            throw new Error(`REACT_APP_AUTH_${provider.toUpperCase()}_CUSTOM_SCOPE is not defined`);
-        }
-        if (!clientId) {
-            throw new Error(`REACT_APP_AUTH_${provider.toUpperCase()}_CLIENT_ID is not defined`);
-        }
         if (!authorizeUrl) {
             throw new Error(
                 `REACT_APP_AUTH_${provider.toUpperCase()}_AUTHORIZE_URL is not defined`
@@ -66,11 +43,6 @@ class AuthUrlProvider {
         }
         if (!wellKnownUrl) {
             throw new Error(`REACT_APP_AUTH_${provider.toUpperCase()}_LOGOUT_URL is not defined`);
-        }
-        if (!tokenToSendToFhirServer) {
-            throw new Error(
-                `REACT_APP_AUTH_${provider.toUpperCase()}_TOKEN_TO_SEND_TO_FHIR_SERVER is not defined`
-            );
         }
 
         return {
